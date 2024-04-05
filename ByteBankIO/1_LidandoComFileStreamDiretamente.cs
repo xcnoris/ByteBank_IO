6314eab0d5d116fcc57e9272d8e67994099dc738
Daniel Portugal <dpcosta@gmail.com>
2024-04-05 15:51:22 -0300

    vídeo 2.3 - Organizando nosso código

---
+++ ByteBankIO/1_LidandoComFileStreamDiretamente.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void LidandoComFileStreamDiretamente()
+    {
+        var enderecoDoArquivo = "contas.txt";
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open, FileAccess.Read))
+        {
+            var buffer = new byte[1024]; // 1KB
+            var numeroDeBytesLidos = -1;
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                // Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
+                EscreverBuffer(buffer, numeroDeBytesLidos);
+            }
+        }
+        Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+        // public override string GetString(byte[] bytes, int index, int count);
+        Console.Write(texto);
+
+        //foreach (var meuByte in buffer)
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
+    }
+}
