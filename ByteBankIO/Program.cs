6242d6d542a5965ae94309240760cf3a1e6e4820
Daniel Portugal <dpcosta@gmail.com>
2024-04-05 10:49:14 -0300

    Alterações da aula 1

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,36 @@
 ﻿using ByteBankIO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open, FileAccess.Read);
 
+        var buffer = new byte[1024]; // 1KB
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+        // public override int Read(byte[] array, int offset, int count);
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+
+        //foreach (var meuByte in buffer)
+        //{
+        //    Console.Write(meuByte);
+        //    Console.Write(" ");
+        //}
     }
 }
