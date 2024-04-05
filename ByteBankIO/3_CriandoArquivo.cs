7a24be8960bac295d9e76f562c3d77e31aa6d6dc
Daniel Portugal <dpcosta@gmail.com>
2024-04-05 16:32:15 -0300

    vídeo 3.3 - Criando arquivo csv

---
+++ ByteBankIO/3_CriandoArquivo.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+        using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456,78945,4785.40,Gustavo Santos";
+            var encoding = Encoding.UTF8;
+            var bytes = encoding.GetBytes(contaComoString);
+
+            fluxoDoArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
