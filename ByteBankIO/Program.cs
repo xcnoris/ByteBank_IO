6d4b41f660425159442e9ceb08d06a6a71e4406d
Daniel Portugal <dpcosta@gmail.com>
2024-04-05 16:02:43 -0300

    vídeo 2.4 - StreamReader

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -2,7 +2,20 @@
 {
     static void Main(string[] args)
     {
-        
+        var enderecoDoArquivo = "contas.txt";
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDoArquivo);
+            // var linha = leitor.ReadLine();
+            // var texto = leitor.ReadToEnd();
+            // var numero = leitor.Read();
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
+        }
         Console.ReadLine();
     }
 }
