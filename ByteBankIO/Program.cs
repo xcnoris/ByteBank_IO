bfb24e398f04ec22c9fd53b32322a5174a47e83f
Daniel Portugal <dpcosta@gmail.com>
2024-04-08 11:49:33 -0300

    vídeo 5.2 - Auxiliares da classe File

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,7 +4,22 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        UsarStreamDeEntrada();
+        //Console.WriteLine("Digite o seu nome:");
+        //string nome = Console.ReadLine();
+
+        var linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+
+        //foreach (var linha in linhas)
+        //{
+        //    Console.WriteLine(linha);
+        //}
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
         Console.WriteLine("Aplicação Finalizada ...");
         Console.ReadLine();
     }
