438a10b6bfb11efd6dbfc40754778d60644f38df
Daniel Portugal <dpcosta@gmail.com>
2024-04-08 11:22:46 -0300

    vídeo 4.3 - BinaryWriter e BinaryReader

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,16 +4,9 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        var caminhoNovoArquivo = "testaEscrita.txt";
-
-        using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
-        using (var escritor = new StreamWriter(fluxoDoArquivo))
-        {
-            escritor.WriteLine(true);
-            escritor.WriteLine(false);
-            escritor.WriteLine(454545454545);
-        }
-        Console.WriteLine("Aplicação finalizada...");
+        EscritaBinaria();
+        LeituraBinaria();
+        Console.WriteLine("Aplicação Finalizada ...");
         Console.ReadLine();
     }
 }
