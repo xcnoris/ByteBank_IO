3657f1d3d5f0513d65027af79ffb7ede88291d03
Daniel Portugal <dpcosta@gmail.com>
2024-04-08 10:22:46 -0300

    vídeo 4.3 - escrita binária

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,7 +4,16 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        TestaEscrita();
+        var caminhoNovoArquivo = "testaEscrita.txt";
+
+        using (var fluxoDoArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDoArquivo))
+        {
+            escritor.WriteLine(true);
+            escritor.WriteLine(false);
+            escritor.WriteLine(454545454545);
+        }
+        Console.WriteLine("Aplicação finalizada...");
         Console.ReadLine();
     }
 }
