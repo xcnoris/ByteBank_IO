d9e04daeb1781e0d2511517f3d23463a33b8257f
Daniel Portugal <dpcosta@gmail.com>
2024-04-08 11:38:23 -0300

    vídeo 5.1 - Stream da console

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,8 +4,7 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        EscritaBinaria();
-        LeituraBinaria();
+        UsarStreamDeEntrada();
         Console.WriteLine("Aplicação Finalizada ...");
         Console.ReadLine();
     }
